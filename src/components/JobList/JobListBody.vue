f795f2ec276bb8c709d89d5db0b509de1fbc3aea
151250121 <151250121@smail.nju.edu.cn>
2018-01-11 10:45:38 +0800

    <dev>:<modify the job filter part>

--- src/components/JobList/JobListBody.vue
+++ src/components/JobList/JobListBody.vue
@@ -2,17 +2,27 @@
 
   <div>
     <single-job v-for="item in 10"></single-job>
+
+    <div class="pagination-wrapper">
+      <el-pagination
+        layout="prev, pager, next"
+        :total="1000">
+      </el-pagination>
+    </div>
+
   </div>
 </template>
 
 <script>
 
   import SingleJob from '../ListItem/SingleJob.vue'
+  import { Pagination } from 'element-ui'
 
   export default {
     name: 'job-list-body',
     components: {
-      SingleJob
+      SingleJob,
+      elPagination: Pagination
     },
     data () {
       return {}
