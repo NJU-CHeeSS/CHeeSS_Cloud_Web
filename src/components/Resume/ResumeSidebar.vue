03e6e1685760e9646406155c13a7f3a4bb04e378
151250121 <151250121@smail.nju.edu.cn>
2018-01-12 14:45:58 +0800

    <dev>:<complete job list part (static)>

--- src/components/Resume/ResumeSidebar.vue
+++ src/components/Resume/ResumeSidebar.vue
@@ -2,6 +2,27 @@
 
   <div class="sidebar-wrapper">
 
+    <div class="resume-header">
+      <p>我的简历</p>
+      <button>修改</button>
+    </div>
+
+    <div class="resume-body">
+      <p>性别：女</p>
+      <p>年龄：20</p>
+      <p>学历：本科</p>
+      <p>所在地：南京</p>
+    </div>
+
+    <div class="resume-body">
+      <p>行业：IT</p>
+      <p>工作经历：2年</p>
+    </div>
+
+    <div class="resume-body">
+      <p>技能：</p>
+    </div>
+
   </div>
 
 </template>
