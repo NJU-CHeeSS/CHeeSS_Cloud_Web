9f33c24f66e8ce4eb9b930441c9684ba020bfe20
Tian0820 <151250121@smail.nju.edu.cn>
2018-03-06 10:52:45 +0800

    <fix>:<fix echarts configuration / fix data connection port>

--- src/components/Chart/skillChart.vue
+++ src/components/Chart/skillChart.vue
@@ -8,26 +8,32 @@
 
   import {mapState, mapActions} from 'vuex'
   import {store} from '../../main'
+  import echarts from 'echarts'
 
-  let echarts = require('echarts/lib/echarts') // 引入基本模板
-  require('echarts/lib/chart/bar') // 引入柱状图组件
-  require('echarts/lib/component/tooltip') // 引入提示框和title组件
-  require('echarts/lib/component/title')
+  //  let echarts = require('echarts/lib/echarts') // 引入基本模板
+  //  require('echarts/lib/chart/bar') // 引入柱状图组件
+  //  require('echarts/lib/component/tooltip') // 引入提示框和title组件
+  //  require('echarts/lib/component/title')
 
   export default {
     name: 'skill-chart',
     components: {},
-    props: {},
     data() {
       return {}
     },
-    mounted() {
-      this.setEchart()
+//    created() {
+//      // this.fetchSkillInfo()
+//      this.setEchart()
+//    },
+    watch: {
+      // 如果路由有变化，会再次执行该方法
+      '$route': 'setEchart'
     },
+    props: ['keywords'],
     computed: {
-      ...mapState('skill', {
-        keywords: state => state.keywords
-      }),
+//      ...mapState('skill', {
+//        keywords: state => state.keywords
+//      }),
       x: {
         get: function () {
           let temp = []
@@ -44,14 +50,17 @@
           for (let i = 0; i < this.keywords.length; i++) {
             temp.push(this.keywords[i].figure)
           }
-
           return temp
         }
       }
     },
     methods: {
+      ...mapActions('skill', [
+        'fetchSkillInfo'
+      ]),
       setEchart() {
         // 基于准备好的dom，初始化echarts实例
+        console.log('set echart!!!')
         let myChart = echarts.init(this.$refs.skillChart)
         // 绘制图表
         myChart.setOption({
@@ -66,21 +75,15 @@
             data: this.y
           }]
         })
-      },
-      ...mapActions('skill', [
-        'fetchSkillInfo'
-      ])
+      }
     },
-    beforeRouterEnter(to, from, next) {
+    mounted() {
+      this.$nextTick(function () {
+        this.setEchart()
+      })
+    },
+    beforeRouteEnter(to, from, next) {
       store.dispatch('fetchSkillInfo').then(next(true))
-    },
-    created() {
-      // this.fetchSkillInfo()
-      this.setEchart()
-    },
-    watch: {
-      // 如果路由有变化，会再次执行该方法
-      // '$route': 'fetchSkillInfo'
-    },
+    }
   }
 </script>
--- src/components/Skill/SkillDistribution.vue
+++ src/components/Skill/SkillDistribution.vue
@@ -2,7 +2,7 @@
 
   <div class="skill-wrapper">
     <div-header :header="'技能要求'"></div-header>
-    <skill-chart></skill-chart>
+    <skill-chart v-if="keywords.length !== 0" :keywords="keywords"></skill-chart>
   </div>
 
 </template>
@@ -11,6 +11,8 @@
 
   import DivHeader from '../Util/DivHeader.vue'
   import SkillChart from '../Chart/skillChart.vue'
+  import {store} from '../../main'
+  import {mapState} from 'vuex'
 
   export default {
     name: 'skill-distribution',
@@ -18,10 +20,18 @@
       DivHeader,
       SkillChart
     },
+    computed: {
+      ...mapState('skill', {
+        keywords: state => state.keywords
+      })
+    },
     data() {
       return {}
     },
-    methods: {}
+    methods: {},
+    beforeRouteEnter(to, from, next) {
+      store.dispatch('fetchSkillInfo').then(next(true))
+    }
   }
 </script>
 
